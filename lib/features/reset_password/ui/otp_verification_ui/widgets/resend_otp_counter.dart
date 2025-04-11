1498bc62323e36179dfb97043667562bbfe89bac
MoBata <mohamedelkarn2003@gmail.com>
2025-04-12 01:08:23 +0200

    Modified The Reset Password Logic for resending the OTP with Some changes on the States and the Ui

---
+++ lib/features/reset_password/ui/otp_verification_ui/widgets/resend_otp_counter.dart
@@ -0,0 +1,24 @@
+part of 'resend_otp_row.dart';
+
+class ResendOtpCounter extends StatelessWidget {
+  const ResendOtpCounter({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocSelector<ResetPasswordCubit, ResetPasswordState, String?>(
+      selector: (state) {
+        return state.maybeMap(
+          otpTimerRunning: (state) => state.remainingSeconds,
+          orElse: () => '00:00',
+        );
+      },
+      builder: (context, remainingSeconds) {
+        return Text(
+          'إعادة الإرسال خلال $remainingSeconds',
+          style: AppTextStyles.cairoSemiOpacityBlackRegular14
+              .copyWith(fontSize: 12),
+        );
+      },
+    );
+  }
+}
